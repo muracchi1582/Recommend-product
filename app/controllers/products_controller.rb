99f2863e4bfc6145ec4a3f9f1d6582c22b51dad3
muracchi1582 <masa1582nh@yahoo.co.jp>
2019-12-13 12:18:59 +0900

    product new make

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,5 +5,21 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
+    @products = Product.limit(4)
+  end
+
+  def create
+    @product = product.new(product_params)
+    @product.save
+    redirect_to root_path
+  end
+
+
+
+  private
+  def product_params
+    params.require(:product).permit(:title, :movie, :image, :price, :information)
+          .merge(user_id: current_user.id)
   end
 end
