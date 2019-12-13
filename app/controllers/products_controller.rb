12d6323850dd2aab458cc41cde701feef71469d4
muracchi1582 <masa1582nh@yahoo.co.jp>
2019-12-13 17:09:53 +0900

    show make

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,9 +10,17 @@ class ProductsController < ApplicationController
   end
 
   def create
-    @product = product.new(product_params)
-    @product.save
-    redirect_to root_path
+    @product = product_url.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @products = Product.order(created_at: :DESC).limit(4)
+    @product = Product.find(params[:id])
   end
 
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
+  mount_uploader :movie, VideoUploader
+  mount_uploader :image, ImageUploader
   belongs_to :user
 end
