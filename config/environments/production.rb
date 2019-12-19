383ed1b1bd0d4dfd1385d159dc10de4577489c16
muracchi1582 <masa1582nh@yahoo.co.jp>
2019-12-19 11:59:14 +0900

    delete

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,6 +93,3 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 end
 
-before_exec do |server|
-  ENV["BUNDLE_GEMFILE"] = File.join(File.expand_path("../../../../", __FILE__), "current", "Gemfile")
-end
