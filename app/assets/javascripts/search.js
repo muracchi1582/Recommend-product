388f21c862110e49397e0a5939d44c72d849d771
muracchi1582 <masa1582nh@yahoo.co.jp>
2019-12-26 19:13:44 +0900

    jquery make

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,22 +1,25 @@
 $(function() {
+  function appendProduct(product){
+    var image = (product.image_url)
+    var title = (product.title)
+    var information = (product.information)
+    var html = `<div class = "search_content__image">
+                  <a href = "/products/:id/search">
+                    <img class = "search__index__image" src = "${image}">
+                  </a>
+                </div>
+                <div class = "search_content__title">
+                  <a class = "title__text" href = "/products/:id">${title}</a>
+                </div>
+                <div class = "search_content__info">
+                  <a class = "info__text" href="/products/:id">${information}</a>
+                </div>`
+      return html;
+  }
 
-  var html = `<div class = "search_content__image">
-                <a href = "/products/:id/search">
-                  <img class = "search__index__image" src = "https://s3-ap-northeast-1.amazonaws.com/uedajyo/uploads/product/image/16/61ab1juvGjL._SL1500_.jpg">
-                </a>
-              </div>
-              <div class = "search_content__title">
-                <a class = "title__text" href = "/products/:id">${product.title}</a>
-              </div>
-              <div class = "search_content__info">
-                <a class = "info__text" href="/products/:id">${product.information}</a>
-              </div>`
-  $(".search-content").append(html);
-
-  
-
-  $(".search__field").on("keyup", function() {
+  $(".search__field").on("keyup", function(e) {
     var input = $(".search__field").val();
+    console.log(input);
     $.ajax({
       type: 'GET',
       url: '/products/search',
@@ -24,8 +27,7 @@ $(function() {
       dataType: 'json'
     })
     .done(function(products) {
-      search_list.empty();
-      $(".search_content").empty();
+      $(".search__field").empty();
       if (products.length !== 0) {
         products.forEach(function(product){
           appendProduct(product);
